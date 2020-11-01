1b1bdbe7216f6ecc0aaf510363a19a1e178494d3
yaser tamim <yassertameem84@yahoo.com>
2020-11-01 22:19:20 +0100

    add js file/submit button

---
+++ Tiger/wwwroot/js/mine.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $("#btn").click(function () {
+        R = $("#inp1").val()
+        D = $("#inp2").val()
+        $("#res1").text(R)
+        $("#res2").text(D)
+        return false;
+    })
+   
+    
+})
